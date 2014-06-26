2b1f7ed4941379db3d0c6f4fe0032619d632ddec
Michael Mendelson <mmendelson@gmail.com>
2014-06-26 07:02:46 -0400

    Message Tree traversal code with test.

---
+++ app/common/message.tree.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Michael on 6/25/2014.
+ */
+'use strict';
+
+var treeData;
+
+//recurse the tree to find a node by id.
+function findNodeById(node, id){
+    if (node.id === id) {
+        return node;
+    } else {
+        if (!node.nodes) return null;  //make sure this has nodes
+        for (var i=0; i<node.nodes.length; i++){
+            var foundNode = findNodeById(node.nodes[i], id);
+            if (foundNode) return foundNode;
+        }
+    }
+    return null; //not found.
+}
+
+exports.nodeById = function(tree, id){
+    return findNodeById(tree, id);
+};
+
+exports.chooseNext = function(node, userInput){
+    if (!node.nodes)
+        throw 'No child nodes.';
+    for (var i=0; i<node.nodes.length; i++){
+        console.log(node.nodes[i].match +  ' '+node.nodes[i].match.test());
+        if (node.nodes[i].match.test(userInput))
+            return node.nodes[i];
+    }
+    return null;
+};
---
+++ app/tests/message.tree.test.js
@@ -0,0 +1,92 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var should = require('should');
+var Tree = require('../common/message.tree');
+
+/**
+ * Globals
+ */
+var tree;
+var data =
+{
+    id: 'root',
+    text: 'Welcome.  Yes or no?',
+    nodes: [
+        {
+            id : 'yes',
+            text: 'You chose yes',
+            match: /yes/i
+        },
+        {
+            id: 'no',
+            text: 'You chose no',
+            match: /no/i
+        },
+        {
+            id: 'maybe',
+            text: 'This is your last chance - ok or you\'re done',
+            match: /maybe/i,
+            nodes: [
+                {
+                    id: 'lastchance',
+                    text: 'Good job!',
+                    match: /ok/i
+                },
+                {
+                    id: 'blewit',
+                    text: 'Too bad',
+                    match: /.*/i
+                }
+            ]
+        }
+    ]
+};
+
+/**
+ * Unit tests
+ */
+describe('Tree structure tests', function() {
+    beforeEach(function(done) {
+        //Initiate the tree.
+        //guess we don't need to do this.
+        done();
+    });
+
+
+    it('gets the appropriate tree node.', function(done) {
+        var node = Tree.nodeById(data, 'yes');
+        node.text.should.equal('You chose yes');
+
+        node = Tree.nodeById(data, 'no');
+        node.text.should.equal('You chose no');
+
+        node = Tree.nodeById(data, 'lastchance');
+        node.text.should.equal('Good job!');
+        done();
+    });
+
+    it('chooses the next node based on user input', function(done) {
+        var node = data; //this is the root node
+        node = Tree.chooseNext(node, 'no');
+        if (!node)
+            throw 'Choice does not match.';
+        ('You chose no').should.equal(node.text);
+
+        var maybeNode = Tree.nodeById(data, 'maybe');
+        var okNode = Tree.chooseNext(maybeNode, 'ok');
+        ('Good job!').should.equal(okNode.text);
+
+        var failNode = Tree.chooseNext(maybeNode, 'oh no');
+        ('Too bad').should.equal(failNode.text);
+
+        done();
+    });
+
+    afterEach(function(done) {
+        done();
+    });
+});
+
